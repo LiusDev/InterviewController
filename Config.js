7d63c0e6c4330654e2b654f5e7882447345cb83c
hoangkm <hoangkm@coccoc.com>
2022-12-31 21:41:25 +0700

    UPDATE: SSheet now open by id instead of url

--- Config.js
+++ Config.js
@@ -1,10 +1,11 @@
 const desksMap = {
   // "deskCode": "deskSS"
+  'Bàn PV 1': SpreadsheetApp.openById('1Cx8smNTm7Jh8V3rCZfyZKeNRtHQOA3Gi3TFE897RRHo')
 };
 const checkinSheetName = 'Check_in';
 const controllingSheetName = 'Điều Phối';
-const checkinSS = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1_EktyzRhUtTURaUPeZpmBO8-qrkWBLNFZtHyF6Nd2ec');
-const controllingSS = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1gpXbVMlSQBAODvia6JCI_y0o-TlgjKJCDDdHHMRkvl0');
+const checkinSS = SpreadsheetApp.openById('1_EktyzRhUtTURaUPeZpmBO8-qrkWBLNFZtHyF6Nd2ec');
+const controllingSS = SpreadsheetApp.openById('1gpXbVMlSQBAODvia6JCI_y0o-TlgjKJCDDdHHMRkvl0');
 
 function put(deskCode, deskSS) {
   desksMap.set(deskCode, deskSS);
