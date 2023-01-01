6960bbad2a2e3d60357c4a66678a374e365d2ffe
hoangkm <hoangkm@coccoc.com>
2023-01-01 16:42:37 +0700

    UDPATE: move js file to seperated folder

---
+++ checkin/Checkin.js
@@ -0,0 +1,37 @@
+function onEdit(event) {
+    const srcSheet = event.source.getActiveSheet();
+
+    if(srcSheet.getName()==='Check_in') {
+        //hide all checked row
+        hideAllCheckedRow(srcSheet);
+        var srcCell = srcSheet.getActiveCell();
+        // nếu có thay đổi giá trị sang true, đưa vào bên Điều Phối
+        if (srcCell.getColumn() === 6 && srcCell.getValue() === true) {
+            var rowNumData = srcCell.getRow();
+            var ID = srcSheet.getRange(rowNumData, 2).getValue();
+            var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(controllingSheetName);
+            var check = 0;
+            var rowNumCol = sheet.getRange('L1');
+            var limit = Number(rowNumCol.getValue()) + 2;
+            for(var i=2; i<limit;i++){
+                if(String(sheet.getRange(i,2).getValue())===ID){
+                    sheet.getRange('A' + i + ':C' + i).setBackground('lime');
+                    sheet.getRange('D' + i + ':I' + i).setBackground('white');
+                    sheet.getRange('J'+i).setValue(0);
+                    check = 1;
+                    break;
+                }
+            }
+            if(check === 0) {
+                moveToSheet(rowNumData);
+            }
+        }
+
+        //nếu thay đổi về false, ẩn hàng đo đi khỏi bên Điều Phối
+        if (srcCell.getColumn() === 6 && srcCell.getValue() === false) {
+            rowNumData = srcCell.getRow();
+            ID = srcSheet.getRange(rowNumData, 2).getValue();
+            reCheck(ID);
+        }
+    }
+}
--- interview_desk/InterviewDesk.js
+++ interview_desk/InterviewDesk.js
@@ -1,32 +1,24 @@
 // TODO find candinate infomation by ID from check_in sheet (ID, Name, Department, Interview time), update to the same row of interview desk sheet
-const checkinSS = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/13cxT8FMWXwsC6VpjlECZwrRvr1NMKpXTm2HR-mxXdzM');
 const checkinSheet = checkinSS.getSheetByName('Check_in');
-const interviewDeskSS = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1Cx8smNTm7Jh8V3rCZfyZKeNRtHQOA3Gi3TFE897RRHo');
-function getSheetById(id) {
-    return SpreadsheetApp.getActive().getSheets().filter(
-        function(s) {return s.getSheetId() === id;}
-    )[0];
-}
-
-var interviewDeskSheet = getSheetById(0);
-
+const interviewDeskSS = desksMap['Bàn PV 1'];
+const interviewDeskSheet = interviewDeskSS.getSheetByName('Bàn PV 1')
 function checkCandinateIdUpdate() {
     var idCols = interviewDeskSheet.getRange('B2:B').getValues();
     idCols.forEach(function(id) {
         if(id[0] !== '') {
-            updateInterviewDesk(id[0], interviewDeskSheet);
+            updateInterviewDesk(id[0]);
         };
     });
 }
 
-function updateInterviewDesk(id, interviewDeskSheet) {
+function updateInterviewDesk(id) {
     var checkinList = checkinSheet.getRange('B2:E').getValues();
     checkinList.forEach(function(checkinCaninate) {
         if(checkinCaninate[0] === id) {
             var name = checkinCaninate[1];
             var department = checkinCaninate[2];
             var interviewTime = checkinCaninate[3];
-            var interviewDeskRow = interviewDeskSheet.getRange('B2:B').getValues().indexOf(id) + 2;
+            var interviewDeskRow = interviewDeskSheet.getRange('B2:B').getValues().indexOf(id) + 3;
             interviewDeskSheet.getRange('C' + interviewDeskRow).setValue(name);
             interviewDeskSheet.getRange('D' + interviewDeskRow).setValue(department);
             interviewDeskSheet.getRange('E' + interviewDeskRow).setValue(interviewTime);
